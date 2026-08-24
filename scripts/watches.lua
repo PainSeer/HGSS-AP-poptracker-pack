6e0785d31228785a073647ddb51f802373d0985b
PainSeer <andrewdiamondhorn@gmail.com>
2026-08-23 23:53:40 -0400

    more plat removal

--- scripts/watches.lua
+++ scripts/watches.lua
@@ -8,14 +8,7 @@ Archipelago:AddBouncedHandler("map handler", onMap)
 
 -- Layout Watches
 ScriptHost:AddWatchForCode("opt_hmreader", "opt_hmreader", toggle_itemgrid)
-ScriptHost:AddWatchForCode("opt_marsh_pass", "opt_marsh_pass", toggle_itemgrid)
-ScriptHost:AddWatchForCode("opt_storage_key", "opt_storage_key", toggle_itemgrid)
-ScriptHost:AddWatchForCode("opt_unown", "opt_unown", toggle_itemgrid)
 ScriptHost:AddWatchForCode("splitmap", "splitmap", toggle_splitmap)
-ScriptHost:AddWatchForCode("opt_pastoria_barriers", "opt_pastoria_barriers", toggle_pastoriabarriers)
-ScriptHost:AddWatchForCode("opt_route_210_lower_barricade", "opt_route_210_lower_barricade", toggle_route210roadblock)
-ScriptHost:AddWatchForCode("opt_route_215_barricade", "opt_route_215_barricade", toggle_route215roadblock)
-ScriptHost:AddWatchForCode("opt_route_207_barricade", "opt_route_207_barricade", toggle_route207roadblock)
 ScriptHost:AddWatchForCode("opt_randomize_fly_items", "opt_randomize_fly_items", toggle_trackerlayout)
 
 -- Pokemon Related
