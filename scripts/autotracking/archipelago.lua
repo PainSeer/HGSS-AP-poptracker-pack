5ede01b3f842c719494e106d1a6d56cfa9577c52
PainSeer <andrewdiamondhorn@gmail.com>
2026-09-18 13:16:13 -0400

    increased the ss aqua values

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -660,9 +660,9 @@ function onMap(mapBounce)
         elseif AQUA_MAPPING[mapID] ~= nil then
             if has("event_first_voyage_completed") then
                 if aqua_direction == "east" then
-                    mapID = mapID + 100
+                    mapID = mapID + 500
                 else
-                    mapID = mapID + 200
+                    mapID = mapID + 600
                 end
             end
 
